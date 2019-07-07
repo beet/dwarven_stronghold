c2a3bbb3364736cb8645b54cb971600b5216ebcd
Mark Beattie <beattie.mark@gmail.com>
2019-07-07 14:33:42 +1000

    Meta-programming for world object predicate methods

--- lib/world_objects/base.rb
+++ lib/world_objects/base.rb
@@ -8,16 +8,20 @@ module WorldObjects
       "  "
     end
 
-    def is_visible?
-      true
-    end
+    # Rather than explicitly defining abstract methods for predicates like
+    # is_wall?, all world objects will return false from any is_foo? method,
+    # allowing only the specific world object to override their own methods to
+    # return true as needed.
+    def method_missing(method, *args, &block)
+      return super(method, *args, &block) unless method.to_s =~ /^is_.+\?$/
 
-    # Some meta-programming using MethodMissing to catch all other predicate
-    # methods like #is_foo? would allow child classes to create whatever they
-    # want, like is_visible? can_open? etc without having to know/define them
-    # all in advance as asbtract methods.
-    def is_wall?
-      false
+      self.class.class_eval do
+        define_method(method) do |*args, &block|
+          false
+        end
+      end
+
+      self.send(method, *args, &block)
     end
   end
 end
