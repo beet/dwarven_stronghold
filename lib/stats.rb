69d978268dbe9982cf306f81fd5af27c5c9a9ab4
Mark Beattie <beattie.mark@gmail.com>
2019-07-13 15:51:21 +1000

    The stats pane shows the world objects in the player's current location.

--- lib/stats.rb
+++ lib/stats.rb
@@ -24,7 +24,8 @@ class Stats
 
   def render
     "Dwarven source beam: #{treasure_distance.round}\n" +
-    "Moves: #{moves}"
+    "Moves: #{moves}\n" + 
+    objects
   end
 
   def found_treasure?
@@ -35,5 +36,22 @@ class Stats
 
   def treasure_distance
     player.location_distance(treasure.location)
+  end
+
+  # Should tell the map to give us the objects at the plauer's location, not
+  # ask for all obects at a location  that is the player's. Except that the
+  # map is agnostic to which of the objects is the player...
+  def objects_at_location
+    map.objects_at_player_location
+  end
+
+  def objects
+    "".tap do |text|
+      objects = objects_at_location
+
+      next if objects.none?
+
+      text << objects.map { |object| "There is a #{object.description} here!" }.join("\n")
+    end
   end
 end
